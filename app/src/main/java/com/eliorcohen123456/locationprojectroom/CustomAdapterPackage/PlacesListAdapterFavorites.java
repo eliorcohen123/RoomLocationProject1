c983bd5bc4ba1b9720ad744ed218b71380231aaf
EliorCohen <elior123456123123@gmail.com>
2019-04-18 20:57:21 +0300

    update

--- app/src/main/java/com/eliorcohen123456/locationprojectroom/CustomAdapterPackage/PlacesListAdapterFavorites.java
+++ app/src/main/java/com/eliorcohen123456/locationprojectroom/CustomAdapterPackage/PlacesListAdapterFavorites.java
@@ -24,8 +24,6 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import com.eliorcohen123456.locationprojectroom.MapsDataPackage.FragmentMapFavorites;
